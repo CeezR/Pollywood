69587140509b66118f3fcafde725ca14c4f8e350
CeezR <cesarforthomme@gmail.com>
2024-12-14 07:16:05 +0100

    chore: project initialization

---
+++ pollywood/src/main/java/dev/cesar/pollywood/PollywoodApplication.java
@@ -0,0 +1,13 @@
+package dev.cesar.pollywood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollywoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollywoodApplication.class, args);
+	}
+
+}
---
+++ pollywood/src/test/java/dev/cesar/pollywood/PollywoodApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cesar.pollywood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollywoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
