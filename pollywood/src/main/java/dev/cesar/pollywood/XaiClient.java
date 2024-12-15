ed6799b237287f6804afcaf4e3815a0fd0fc22a4
CeezR <cesarforthomme@gmail.com>
2024-12-15 10:58:52 +0100

    feat: request body added to xai request

--- pollywood/src/main/java/dev/cesar/pollywood/XaiClient.java
+++ pollywood/src/main/java/dev/cesar/pollywood/XaiClient.java
@@ -1,6 +1,7 @@
 package dev.cesar.pollywood;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -16,10 +17,11 @@ public class XaiClient {
         this.restClient = restClientBuilder.build();
     }
 
-    public String propt() {
+    public String propt(XaiRequest request) {
         return restClient
-                .get()
+                .method(HttpMethod.GET)
                 .uri("https://api.x.ai/v1/chat/completions")
+                .body(request)
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Bearer " + apiKey)
                 .retrieve()
