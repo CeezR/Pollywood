f374e4a3ddb964ad79049d90f0625155c6682215
CeezR <cesarforthomme@gmail.com>
2024-12-15 12:50:27 +0100

    feat: changed return type from string --> Message.class

--- pollywood/src/main/java/dev/cesar/pollywood/view/XaiClient.java
+++ pollywood/src/main/java/dev/cesar/pollywood/view/XaiClient.java
@@ -1,10 +1,14 @@
 package dev.cesar.pollywood.view;
 
+import dev.cesar.pollywood.model.Message;
 import dev.cesar.pollywood.model.XaiRequest;
+import dev.cesar.pollywood.model.XaiResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import java.util.Objects;
 
 @Service
 public class XaiClient {
@@ -18,15 +22,18 @@ public class XaiClient {
         this.restClient = restClientBuilder.build();
     }
 
-    public String prompt(XaiRequest request) {
-        return restClient
-                .method(HttpMethod.GET)
-                .uri("https://api.x.ai/v1/chat/completions")
-                .body(request)
-                .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer " + apiKey)
-                .retrieve()
-                .body(String.class);
+    public Message prompt(XaiRequest request) {
+        return Objects.requireNonNull(restClient
+                        .method(HttpMethod.GET)
+                        .uri("https://api.x.ai/v1/chat/completions")
+                        .body(request)
+                        .header("Content-Type", "application/json")
+                        .header("Authorization", "Bearer " + apiKey)
+                        .retrieve()
+                        .body(XaiResponse.class))
+                .choices()
+                .getFirst()
+                .message();
 
     }
 }
